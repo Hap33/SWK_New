6b4d4e3dc5f0b27a4cb0f3a1d1d333969e1e925d
MSI\Maxime <megamanjpn@gmail.com>
2018-06-12 10:36:53 +0200

    Updated

--- Assets/Code Multi/J1Camera.cs
+++ Assets/Code Multi/J1Camera.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class J1Camera : MonoBehaviour {
-
+    public Camera Cam;
 	void Update()
     {
         if (MultiHowMany.Singleton.PlayerNumber == 2)
         {
-
+            Cam.rect = new Rect(0, 0.5f, 1, 0.5f);
+        }
+        else
+        {
+            Cam.rect = new Rect(0, 0.5f, 1, 0.5f);
         }
     }
 }
--- Assets/Code Multi/J2Camera.cs
+++ Assets/Code Multi/J2Camera.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class J2Camera : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Camera Cam;
+    void Update()
+    {
+        if (MultiHowMany.Singleton.PlayerNumber == 2)
+        {
+            Cam.rect = new Rect(0, 0, 1, 0.5f);
+        }
+        else
+        {
+            Cam.rect = new Rect(0, 0.5f, 1, 0.5f);
+        }
+    }
 }
