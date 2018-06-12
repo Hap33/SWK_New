5cf9a99ca5725e5224e6cec9af8df71e2fdcda5e
MSI\Maxime <megamanjpn@gmail.com>
2018-06-12 10:23:22 +0200

    Récup

---
+++ Assets/Code Multi/J1Camera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J1Camera : MonoBehaviour {
+
+	void Update()
+    {
+        if (MultiHowMany.Singleton.PlayerNumber == 2)
+        {
+
+        }
+    }
+}
